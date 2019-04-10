bedc67006205b487475877d65b4346addd1f1bd2
Sang Le <lesan6021@gmail.com>
2019-04-10 18:43:37 -0400

    Add script for index

---
+++ heehaw.js
@@ -0,0 +1,21 @@
+for(var i = 1; i <= 100; i++) {
+	var hee = i/3;
+	var haw = i/5;
+
+	if(Number.isInteger(hee) && Number.isInteger(haw)) {
+		console.log("Hee Haw!");
+		document.write("Hee Haw!<br>");
+	}
+	else if(Number.isInteger(hee) && !Number.isInteger(haw)) {
+		console.log("Hee!");
+		document.write("Hee!<br>")
+	}
+	else if(Number.isInteger(hee)&& !Number.isInteger(haw)) {
+		console.log("Haw!");
+		document.write("Haw!<br>");
+	}
+	else {
+		console.log(i);
+		document.write(i + "<br>");
+	}
+}
