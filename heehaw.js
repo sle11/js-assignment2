3af270c55fc1766a8a15d7185b2f3c2a8d2c82eb
Sang Le <lesan6021@gmail.com>
2019-04-17 20:32:46 -0400

    Modify to be inside a function

--- heehaw.js
+++ heehaw.js
@@ -1,21 +1,29 @@
-for(var i = 1; i <= 100; i++) {
-	var hee = i/3;
-	var haw = i/5;
+function heeHaw() {
 
-	if(Number.isInteger(hee) && Number.isInteger(haw)) {
-		console.log("Hee Haw!");
-		document.write("Hee Haw!<br>");
+	var userInput = document.getElementById('num').value;
+
+	for(var i = 1; i <= userInput; i++) {
+		var hee = i/3;
+		var haw = i/5;
+
+		if(Number.isInteger(hee) && Number.isInteger(haw)) {
+			console.log("Hee Haw!");
+			document.write("Hee Haw!<br>");
+		}
+		else if(Number.isInteger(hee) && !Number.isInteger(haw)) {
+			console.log("Hee!");
+			document.write("Hee!<br>")
+		}
+		else if(Number.isInteger(hee)&& !Number.isInteger(haw)) {
+			console.log("Haw!");
+			document.write("Haw!<br>");
+		}
+		else {
+			console.log(i);
+			document.write(i + "<br>");
+		}
 	}
-	else if(Number.isInteger(hee) && !Number.isInteger(haw)) {
-		console.log("Hee!");
-		document.write("Hee!<br>")
-	}
-	else if(Number.isInteger(hee)&& !Number.isInteger(haw)) {
-		console.log("Haw!");
-		document.write("Haw!<br>");
-	}
-	else {
-		console.log(i);
-		document.write(i + "<br>");
-	}
+	
 }
+
+
